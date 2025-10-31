9136af2fa11a612ddee64245c1cd4e6b3281e184
harshsancheti11-jpg <harshsancheti.11@gmail.com>
2025-10-31 22:26:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,13 @@ st.title("🏦 Indian Banking KPI Screener (2020–2025)")
 # Sidebar filters
 st.sidebar.header("🔍 Filter")
 banks = st.sidebar.multiselect("Select Banks", sorted(df["bank_name"].unique()), default=["HDFC Bank"])
-years = st.sidebar.multiselect("Select Years", sorted(df["year"].unique()), reverse=True, default=[2021, 2020])
+df["year"] = df["year"].astype(str)
+years = st.sidebar.multiselect(
+    "Select Years",
+    sorted(df["year"].unique(), reverse=True),
+    default=["2021", "2020"]
+)
+
 metric = st.sidebar.selectbox("Select Metric", [
     "deposits","casa_ratio","aum","gnpa","nnpa","pat","nim","roa",
     "cost_to_income","capital_adequacy_ratio","liquidity_coverage_ratio",
